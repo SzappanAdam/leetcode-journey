6c8fe8350b9f2b1d9b36e7ded9d309b8bde0c1c5
SzappanAdam <szappanadam@gmail.com>
2026-08-03 17:12:51 +0200

    Continue #228

--- easy/228-summary-ranges/summary_ranges.py
+++ easy/228-summary-ranges/summary_ranges.py
@@ -1,15 +1,25 @@
-def summary_ranges(nums: list[int])->list[int]:
+def summary_ranges(nums: list[int])->list[str]:
+    if not nums:
+        return []
+
     start = nums[0]
     territories = []
 
-    for i in range(len(nums) + 2):
-        if start+1 == nums[i+1]:
-            territories.append(nums[i])
-        start = nums[i]
-        print(start)
+    for i in range(len(nums)):
+        if nums[i] == nums[-1]:
+            if start == nums[i]:
+                territories.append(str(start))
+            else:
+                territories.append(f"{start}->{nums[i]}")
+        elif nums[i] + 1 != nums[i + 1]:
+            if start == nums[i]:
+                territories.append(str(start))
+            else:
+                territories.append(f"{start}->{nums[i]}")
+            start = nums[i+1]
+    return territories
 
-print(summary_ranges([0, 1, 2, 4, 5, 7])) #["0->2", "4->5", "7"]
-print(summary_ranges([0, 2, 3, 4, 6, 8, 9])) #["0", "2->4", "6", "8->9"]
+print(summary_ranges([0, 1, 2, 4, 5, 7]))
+print(summary_ranges([0, 2, 3, 4, 6, 8, 9]))
 print(summary_ranges([3, 4, 5, 8, 10, 11, 12]))
-
-
+print(summary_ranges([]))
